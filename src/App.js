851c3c159b6e9724b52e193f0d88f9ddb471fc6a
nghia.nguyen <nghiant9068@gmail.com>
2020-03-07 19:48:09 +0700

    Challenge 1 done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: {
+				items: []
+			},
+			viewData: [],
+			searchTerm: '',
+		};
+		this.onTyping = this.onTyping.bind(this);
+	}
+
+	componentDidMount() {
+		var proxyUrl = 'https://cors-anywhere.herokuapp.com/',
+			targetUrl = 'https://www.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=true'
+		fetch(proxyUrl + targetUrl)
+		.then(blob => blob.json())
+		.then(data => {
+			this.setState({
+				data: data,
+				viewData: data.items,
+			});
+		})
+		.catch(e => {
+			console.log(e);
+		});
+	}
+
+	onTyping(e) {
+		this.setState({
+			viewData: this.filterContent(this.state.data.items, e.target.value),
+		});
+	}
+
+	filterContent(data = [], searchTerm = '') {
+		if (!searchTerm) return data;
+		searchTerm = searchTerm.trim();
+		const result = data.filter(item => this.includesTag(item, searchTerm) || this.includesAuthor(item, searchTerm) || this.includesImgOrHiResImg(item, searchTerm));
+		return result;
+	}
+
+	includesTag(item = { tags: ''}, searchTerm = '') {
+		searchTerm = searchTerm.toLowerCase();
+		return item.tags.toLowerCase().includes(searchTerm);
+	}
+
+	includesAuthor(item = { author: ''}, searchTerm = '') {
+		searchTerm = searchTerm.toLowerCase();
+		return item.author.toLowerCase().includes(searchTerm);
+	}
+
+	includesImgOrHiResImg(item = { description: '' }, searchTerm = '') {
+		const includeInformationPart = item.description.slice(item.description.indexOf('</p>'));
+		const hiResImgPart = includeInformationPart.slice(0, includeInformationPart.indexOf('</a>'));
+		const imgPart = includeInformationPart.slice(includeInformationPart.indexOf('<img'));
+
+		const hiResImgLink = hiResImgPart.split('"')[1];
+		const imgLink = imgPart.split('"')[1];
+
+		return hiResImgLink.includes(searchTerm) || imgLink.includes(searchTerm);
+	}
+
+	generateContent(data = []) {
+		const itemJsx = item => `${item.description}<p><b>tags: ${item.tags.replace(/ /g, ', ')}</b></p><hr />`;
+		return Array.isArray(data) ? data.map((item, idx) => <div key={idx} dangerouslySetInnerHTML={{__html: itemJsx(item)}}></div>) : <div></div>;
+	}
+
+  	render() {
+		const content = this.generateContent(this.state.viewData);
+		return (
+			<div className="App">
+				<div className="search-input">
+					<input type="text" onKeyUp={this.onTyping} />
+				</div>
+				<div className="content">
+					{content}
+				</div>
+			</div>
+		)
+	}
 }
 
 export default App;
